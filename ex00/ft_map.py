1fdad4b9eb2b4b7c8fabc66c5e7fd04613243b81
Matteo Fernandes <mfernand@f1r3s1.clusters.42paris.fr>
2025-10-28 17:49:44 +0100

    ex00 wip

--- ex00/ft_map.py
+++ ex00/ft_map.py
@@ -1,3 +1,8 @@
+from curses import flash
+from email import iterators
+from typing import Iterator
+
+
 def ft_map(function_to_apply, iterable):
     """Map the function to all elements of the iterable.
     Args:
@@ -7,14 +12,31 @@ def ft_map(function_to_apply, iterable):
     An iterable.
     None if the iterable can not be used by the function.
     """
+    valid:False
+    if isinstance(iterable, list) == True:
+        valid = True
+    if isinstance(iterable, tuple) == True:
+        valid = True
+    if isinstance(iterable, Iterator) == True:
+        valid = True
+
+    if valid == False:
+        raise Exception("ERROR")
+    
+    res:list = []
+
+    res.append(function_to_apply(elt) for elt in iterable)
+    return res
 
 
-
-# # Example 1:
-# x = [1, 2, 3, 4, 5]
-# ft_map(lambda dum: dum + 1, x)
-# # Output:
+# Example 1:
+x = [1, 2, 3, 4, 5]
+ft_map(lambda dum: dum + 1, x)
+# Output:
 # <generator object ft_map at 0x7f708faab7b0> # The adress will be different
-# list(ft_map(lambda t: t + 1, x))
-# # Output:
+
+res = ft_map(lambda t: t + 1, x)
+print(res)
+print(list(res))
+# Output:
 # [2, 3, 4, 5, 6]
